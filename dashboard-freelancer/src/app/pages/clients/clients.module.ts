06fcfca123965c900f18ab3e01e89d9891c33446
DNZI <david.nzi@epsi.fr>
2018-09-12 16:16:39 +0200

    create client page exemple

--- dashboard-freelancer/src/app/pages/clients/clients.module.ts
+++ dashboard-freelancer/src/app/pages/clients/clients.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { ClientsComponent } from './clients.component';
 import { ButtonSwitchComponent } from './button-switch/button-switch.component';
+import { InputComponent } from './input/input.component';
 
 
 const components = [
@@ -18,6 +19,7 @@ const components = [
   ],
   declarations: [
   	...components,
+  	InputComponent,
   ],
 })
 export class ClientsModule { }
---
+++ dashboard-freelancer/src/app/pages/clients/input/input.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-input',
+  styleUrls: ['./input.component.scss'],
+  template: `
+    <p>
+  input works!
+</p>
+  `,
+})
+export class InputComponent {
+
+}
